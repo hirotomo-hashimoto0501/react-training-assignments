335be8a754077c1d3b3dd8b3ef7da8bbf0f9cdd2
hirotomo-hashimoto <60131587+hirotomo-hashimoto@users.noreply.github.com>
2026-09-01 21:17:33 +0900

    todoリストに削除ボタンを追加

--- 04-react-todo/src/components/TodoItem.tsx
+++ 04-react-todo/src/components/TodoItem.tsx
@@ -1,15 +1,36 @@
 import type { Todo } from '../types';
 
 type TodoItemProps = {
+  // 表示対象のTodo1件分のデータ
   todo: Todo;
+  // 削除ボタンが押されたときに呼び出す関数
+  // 「どのTodoを削除するか」はこのコンポーネントが一番知っているので、
+  // 自分のidを添えて親（TodoList）に削除を依頼する形にする
+  onDelete: (id: number) => void;
 };
 
-// 現時点ではTodo追加機能のみを実装するため、テキスト表示のみを行う。
-// 削除・修正ボタンは、それぞれの機能を実装するステップで追加する。
-function TodoItem({ todo }: TodoItemProps) {
+// Todo追加機能に続いて削除機能を実装するステップ。
+// 各Todoに削除ボタンを表示し、押されたら自分のidを親に伝える。
+function TodoItem({ todo, onDelete }: TodoItemProps) {
+  // 削除ボタンのクリックハンドラ
+  // 実際に配列から取り除く処理はTodoList側が持っているため、
+  // ここでは「このidのTodoを削除してほしい」と伝えるだけでよい
+  const handleDeleteClick = (): void => {
+    onDelete(todo.id);
+  };
+
   return (
     <li className="todo-item">
       <span className="todo-text">{todo.text}</span>
+
+      {/* 削除ボタン：押すとこのTodoだけが一覧から消える */}
+      <button
+        type="button"
+        className="delete-button"
+        onClick={handleDeleteClick}
+      >
+        削除
+      </button>
     </li>
   );
 }
--- 04-react-todo/src/components/TodoList.tsx
+++ 04-react-todo/src/components/TodoList.tsx
@@ -19,12 +19,25 @@ function TodoList() {
     setNextId((prevId) => prevId + 1);
   };
 
+  // 指定したidのTodoだけを配列から取り除く関数
+  const handleDeleteTodo = (id: number): void => {
+    // Array.filter は「条件に合う要素だけを残した新しい配列」を作るメソッド。
+    // ここでは「削除対象のidと一致しないTodo」だけを残すことで、
+    // 結果的に対象のTodoだけが取り除かれた配列になる。
+    // 元のtodos配列自体は書き換えない（Reactの状態更新は常に新しい値を渡す）。
+    setTodos((prevTodos) =>
+      prevTodos.filter((todo) => todo.id !== id)
+    );
+  };
+
   return (
     <div className="todo-list-wrapper">
       <TodoInput onAdd={handleAddTodo} />
       <ul className="todo-list">
         {todos.map((todo) => (
-          <TodoItem key={todo.id} todo={todo} />
+          // key: Reactがリストの各要素を区別するための一意な値
+          // onDelete: 削除ボタン押下時にhandleDeleteTodoを呼び出せるように渡す
+          <TodoItem key={todo.id} todo={todo} onDelete={handleDeleteTodo} />
         ))}
       </ul>
     </div>
